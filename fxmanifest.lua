cd5a3448f3d457f940b130b1b372d7200ae4774d
ESX Developer Portugal <88593228+zcmg@users.noreply.github.com>
2022-11-20 23:06:40 +0000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,24 @@ fx_version 'cerulean'
 games { 'gta5' }
 
 author 'zcmg#5307'
-description 'v1.1'
+description 'v1.4'
 
-shared_scripts {
-	'config.lua',
-	'@es_extended/imports.lua'
+ui_page 'html/ui.html'
+
+shared_scripts{
+	'config.lua'
 }
 
 client_scripts {
-	'client/*.lua'
+	'client.lua'
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/*.lua'
+	'server.lua'
 }
+
+files {
+	'html/*.*'
+}
+
+export 'Alerta'
